dea8a4751ebff6d9fde452bbb4d8d4fb1c0227fa
Virufighter <alvinpoulose4@gmail.com>
2022-11-24 12:49:05 +0530

    viewall

---
+++ src/app/viewallpatients/viewallpatients.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewallpatients',
+  templateUrl: './viewallpatients.component.html',
+  styleUrls: ['./viewallpatients.component.css']
+})
+export class ViewallpatientsComponent {
+
+}
