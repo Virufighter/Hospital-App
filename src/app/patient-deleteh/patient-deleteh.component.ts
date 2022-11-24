412e8aeab3b31d4a6b8e2b977d3ffd9927276296
Virufighter <alvinpoulose4@gmail.com>
2022-11-24 11:54:05 +0530

    delete

---
+++ src/app/patient-deleteh/patient-deleteh.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-deleteh',
+  templateUrl: './patient-deleteh.component.html',
+  styleUrls: ['./patient-deleteh.component.css']
+})
+export class PatientDeletehComponent {
+
+}
